25a73fc72ca5f028f2eeb4b9029f0c6f4e19138d
majidalykhan <majidalykhan@gmail.com>
2020-02-11 22:19:05 +0500

    Needle rotation

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.stopwatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton gs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gs = findViewById(R.id.getStart);
+
+
+        gs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Stopwatch.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/stopwatch/Stopwatch.java
+++ app/src/main/java/com/example/stopwatch/Stopwatch.java
@@ -3,12 +3,43 @@ package com.example.stopwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.util.Date;
 
 public class Stopwatch extends AppCompatActivity {
+
+    ImageButton start, finish;
+    ImageView needle;
+    Animation rounding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+
+        start = findViewById(R.id.start);
+        finish = findViewById(R.id.finish);
+        needle = findViewById(R.id.needle);
+
+        //Load animation
+        rounding = AnimationUtils.loadAnimation(this, R.anim.rounding);
+
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                needle.startAnimation(rounding);
+
+            }
+        });
+
     }
+
 }
